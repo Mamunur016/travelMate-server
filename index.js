d3db31902993909edb473a7e050e25f23f54b4a0
Mamunur1 <mamun723548@gmail.com>
2021-10-31 01:40:10 +0600

    api

--- index.js
+++ index.js
@@ -23,20 +23,19 @@ async function run() {
 
         const database = client.db('travelMate');
         const toursCollection = database.collection('tours');
+        const ordersCollection = database.collection('orders');
 
 
-
+        // GET API FOR TOURS
         app.get('/tours', async (req, res) => {
             const cursor = toursCollection.find({});
             const tours = await cursor.toArray();
             res.json(tours);
         });
 
-        app.get('/hello', async (req, res) => {
-            res.send('ki vai hoy na');
-        })
 
-        // GET SINGLE Tour api
+
+        // GET API FOR SINGLE Tour
 
         app.get('/tours/:id', async (req, res) => {
             const id = req.params.id;
@@ -44,6 +43,63 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const tour = await toursCollection.findOne(query);
             res.json(tour);
+        });
+
+        // POST API FOR TOUR
+        app.post('/tours', async (req, res) => {
+            const tour = req.body;
+            console.log('hit the post api', tour);
+
+            const result = await toursCollection.insertOne(tour);
+            console.log(result);
+            res.json(result)
+        });
+
+        // Add Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+        });
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.json(orders)
+        });
+
+        app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log('getting email', email);
+            const query = { email: email };
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.json(orders);
+        });
+
+        //UPDATE API
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(req.body);
+            const updatedOrder = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: 'Approved ',
+
+                },
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc, options)
+            console.log('updating', id)
+            res.json(result)
+        })
+
+        // DELETE API
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
         })
 
 
